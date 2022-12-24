24a3768810c329e58fd081b0ffe7f8464f25e42d
choonsik <108234111+choons1k2@users.noreply.github.com>
2022-12-24 14:53:01 +0900

    Add files via upload

--- test3.java
+++ test3.java
@@ -19,6 +19,34 @@ public class test3 {
 			c[backOfC] = arr[i];
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	/*
+	아래배열들을 반으로 나누고 서로 같은모양인지 판별하시오.	
+	같은모양이면 true , 다른모양이면 false 출력
+	[예시]
+		154451 은 절반으로 나누면 서로 같은모양이다.
+		154751 은 절반으로 나누면 서로 다른모양이다.
+ */
+int arr1[] = {1,5,4,4,5,1};	
+int arr2[] = {1,5,4,7,5,1};
+	boolean result = false;
+	for(int i=0; i<4; i++) {
+		if(arr1[i]==arr[5-i])
+			result = true;
+		else
+			result = false;
+		
+		
+	}
+	
+	System.out.println(result);
+	
 
 	}
 
